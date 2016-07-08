66e41cada28033b2fa6ff24b7397a29f06659458
ravsom <ravikanth.somayaji@gmail.com>
2016-07-08 08:36:28 +0530

    Formatted code and adding facebook button

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
 	resolve: {
 		extensions: ['', '.js', '.jsx']
 	},
-	devtool: "source-map",
+	//devtool: "source-map",
 	devServer: {
 		historyApiFallback: true,
 		contentBase: './public'
